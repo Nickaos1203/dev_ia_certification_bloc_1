842a5490c31868de68e5cfca200858e542419ace
Nickaos1203 <nicolas.guillon95@yahoo.com>
2026-08-10 01:47:47 +0200

    MaJ fichier spider

--- web_scraping/web_scraping/spiders/videogames_spider.py
+++ web_scraping/web_scraping/spiders/videogames_spider.py
@@ -4,20 +4,42 @@ import scrapy
 
 
 class VideogamesSpider(scrapy.Spider):
-    name= "dishe_spider"
-    allowed_domains = ["metacritic.com"]
-    start_urls = ["https://www.metacritic.com/browse/game/?releaseYearMin=2025&releaseYearMax=2026&page=" + str(x) for x in range(1, 10)]
+    name= "videogames_spider"
+
 
     async def start(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        pages_urls = ["https://www.metacritic.com/browse/game/?releaseYearMin=2025&releaseYearMax=2026&page=" + str(x) for x in range(1, 5)]
+        for url in pages_urls:
+            yield scrapy.Request(
+                url=url, 
+                callback=self.parse)
+
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        urls = response.css('div[data-testid="filter-results"] a::attr(href)').getall()
+        
+        for url in urls:
+            yield response.follow(
+                url,
+                callback=self.parse_videogames
+            )
+
+
+    def parse_videogames(self, response):
+        yield {
+            "url" : response.url
+        }
+
+
+        # url = ""
+        # nom = ""
+        # date = ""
+        # plateformes = ""
+        # editeur = ""
+        # genre = ""
+        # resume = ""
+        # note_metascore = ""
+        # note_utilisateur = ""
+        # pourcentage_note_positif = ""
+        # pourcentage_note_negatif = ""
+        # pourcentage_note_mixte = ""
